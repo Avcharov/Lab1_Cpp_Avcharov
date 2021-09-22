972ffe9a50668aa65ac70da7a95bab44de7bc5a4
Admin <Admin@XIAOMI>
2021-09-22 09:50:10 +0300

    Добавьте файлы проекта.

---
+++ Lab1_Cpp_Avcharov/Lab1_Cpp_Avcharov.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <math.h> 
+#include <cmath>  
+using namespace std;
+#define PI 3.14159265  
+int main() {
+    int a, b;
+    int kut;
+    cin >> a >> b >> kut;
+    cout << sqrt(a * a + b * b - 2 * a * b * cos(kut * PI / 180)) << endl;
+
+    return 0;
+}
+
+
